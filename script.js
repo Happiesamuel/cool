afcf50de70195a1b91fc31ef84349557e96b2cb7
Happiesamuel <114792014+Happiesamuel@users.noreply.github.com>
2023-02-02 10:25:27 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+const ul = document.querySelector("ul");
+const icon = document.querySelector("#icon");
+icon.addEventListener("click", function () {
+  ul.classList.toggle("show");
+});
